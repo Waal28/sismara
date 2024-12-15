fc134eb4dcf726b2b720ee47d9ffe240e78f364e
IwaL FaizuL <iwalfaizul@gmail.com>
2024-12-15 22:53:47 +0700

    fix api acara

--- api/src/acara.js
+++ api/src/acara.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-useless-catch */
-import axios from "axios";
 import Agent from "../agent";
 
 export async function getAllEvents() {
@@ -33,30 +32,9 @@ export async function getAllEventsByFilters({
     throw error;
   }
 }
-export async function getEventsWithLimit({ limit, skip, keyword = "" }) {
-  // const response = await Agent.get(`/galeri/album?limit=${limit}&skip=${skip`);
-  const response = await axios.get(
-    `https://dummyjson.com/products/search?q=${keyword}&limit=${limit}&skip=${skip}`
-  );
-  return response;
-}
 export async function getOneEvent(idEvent) {
   // const response = await Agent.get(`/galeri/album?limit=${limit}&skip=${skip`);
   const response = await Agent.get(`/acara/${idEvent}`);
-  return response;
-}
-export async function getLatestFeedBack(limit, skip) {
-  // const response = await Agent.get(`/galeri/album?limit=${limit}&skip=${skip`);
-  const response = await axios.get(
-    `https://dummyjson.com/products?limit=${limit}&skip=${skip}`
-  );
-  return response;
-}
-export async function getEventReview(idEvent) {
-  // const response = await Agent.get(`/galeri/album?limit=${limit}&skip=${skip`);
-  const response = await axios.get(
-    `https://dummyjson.com/products?limit=10&skip=0`
-  );
   return response;
 }
 export async function addEvent(data) {
